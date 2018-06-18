58e5e11120c2d13f941c5526adb7d90439755866
zhangzhu <zhangzhu_mail@163.com>
2018-06-19 01:00:32 +0800

    unity shader first

---
+++ Project/unitypbr/mdkmaterialGUI.cs
@@ -0,0 +1,9 @@
+using UnityEditor;
+
+public class MdkMaterialGUI : ShaderGUI
+{
+    public override void OnGUI (MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        base.OnGUI (materialEditor, properties);
+    }
+}
